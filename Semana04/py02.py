73c6964b162aec18c3c6176ba7fbd3cbb379f16f
Rafael <bboyrafinhazika@ufu.br>
2022-05-29 11:55:33 -0300

    Exercício 1 - Seção 02

--- Semana04/py02.py
+++ Semana04/py02.py
@@ -1,1 +1,20 @@
-print('hello w')
+print('hello wolrd')
+
+str = 'Rafael Dias Pereira'
+
+print(str[0:6])
+
+str2 = str.replace('Dias', 'Days')
+
+print(str2)
+
+str3 = '{}, {}!'.format('hello', str2[0:11])
+
+print(str3)
+
+nome = 'Rafael'
+sobrenome = 'Days'
+
+print(f'hello, {nome.upper()} {sobrenome.lower()}!')
+
+print(dir(nome))
