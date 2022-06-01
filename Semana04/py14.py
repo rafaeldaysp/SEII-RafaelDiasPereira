f07a91240fac414fcc55105289219d3ce0526ced
Rafael <bboyrafinhazika@ufu.br>
2022-05-31 21:27:06 -0300

    Adicionando arquivos

---
+++ Semana04/py14.py
@@ -0,0 +1,21 @@
+import os
+import random
+
+try:
+    os.mkdir('teste')
+except:
+    pass
+
+os.chdir(os.getcwd() + '/teste')
+
+for i in range(10):
+    with open(random.choice(('teste-', 'testando-', 'nome-', 'arquivo-')) + str(random.randint(1, 10)) + '.txt', 'w') as f:
+        f.write('Rafael')
+
+for f in os.listdir():
+    f_name, f_ext = os.path.splitext(f)
+    f_title, f_num = f_name.split('-')
+    f_num.zfill(2)
+    novo_formato = '{}-{}{}'.format(f_num, f_title, f_ext)
+    os.rename(f, novo_formato)
+    
