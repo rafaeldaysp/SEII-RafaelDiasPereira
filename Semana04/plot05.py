166ad552a4b8336999be5d4bcc2f90dade6c03a5
rafael <bboyrafinhazika@ufu.br>
2022-06-02 00:09:20 -0300

    Adicionando arquivos

---
+++ Semana04/plot05.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0, 1000, 1)
+x2 = np.arange(-1000, 1000, 1)
+
---
+++ Semana04/plot06.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from plot05 import x, x2
+
+plt.plot(x, x**2)
+plt.show()
+
+plt.plot(x2, x2**2)
+plt.show()
