901a5952331b56aa9b545f20557224e7f0cdc5d2
rafaeldaysp <bboyrafinhazika@ufu.br>
2022-06-15 14:25:56 -0300

    Comentando códigos socket UDP e TCP

--- Semana06_Socket/Transfers_Files/transfer3.py
+++ Semana06_Socket/Transfers_Files/transfer3.py
@@ -9,12 +9,12 @@ file_name = sys.argv[1]
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-sock.sendto(file_name, (UDP_IP, UDP_PORT))      # envia um nome de arquivo no protocolo UDP
+sock.sendto(file_name, (UDP_IP, UDP_PORT))      # envia um nome de arquivo em pacotes UDP
 print ("Sending %s ..." % file_name)
 f = open(file_name, "r")
 data = f.read(buf)
 while(data):
-    if(sock.sendto(data, (UDP_IP, UDP_PORT))):      # envia o conteúdo do arquivo no protocolo UDP
+    if(sock.sendto(data, (UDP_IP, UDP_PORT))):      # envia o conteúdo do arquivo em pacotes UDP
         data = f.read(buf)
         time.sleep(0.02) 
 
--- Semana06_Socket/Transfers_Files/transfer4.py
+++ Semana06_Socket/Transfers_Files/transfer4.py
@@ -6,11 +6,11 @@ IN_PORT = 5005                      # Define a porta UDP
 timeout = 3
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)     # inicializa um socket no protocolo UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)     # inicializa um socket em pacotes UDP
 sock.bind((UDP_IP, IN_PORT))                                # define o socket como um servidor
 
 while True:
-    data, addr = sock.recvfrom(1024)                        # recebe os dados do client no protocolo UDP
+    data, addr = sock.recvfrom(1024)                        # recebe os dados do client em pacotes UDP
     if data:
         print ("File name:", data)
         file_name = data.strip()
@@ -20,7 +20,7 @@ while True:
     while True:
         ready = select.select([sock], [], [], timeout)
         if ready[0]:
-            data, addr = sock.recvfrom(1024)               # recebe os dados do client no protocolo UDP
+            data, addr = sock.recvfrom(1024)               # recebe os dados do client em pacotes UDP
             f.write(data)
         else:
             print ("%s Finish!" % file_name)
