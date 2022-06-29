0dc11276fd470724999d23d6c07452e1b7c1c6bb
Rafael <bboyrafinhazika@ufu.br>
2022-06-29 10:02:23 -0300

    Calculadora front-end

---
+++ Semana08/Calculadora/calculadora.js
@@ -0,0 +1,18 @@
+var visor
+var resultado = ""
+function ler_botao(num){
+    visor = window.document.calc.visor.value += num
+    
+}
+function apagar(){
+    window.document.calc.visor.value = ""
+}
+
+function calcular(){
+    resultado = eval(visor)
+    window.document.calc.visor.value = resultado
+}
+
+function ans(){
+    ler_botao(resultado)
+}
